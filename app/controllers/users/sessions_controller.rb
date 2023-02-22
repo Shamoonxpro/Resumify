a94abf2dc12244ffa8c1ab77ce67f14b9bb448e8
Shamoonxpro <shamoon.khan@xprolabs.com>
2023-02-22 19:38:52 +0300

    Removed the extra spaces

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
   prepend_before_action :check_recaptcha, only: [:create] # Change this to be any actions you want to protect.
-
   def create
     self.resource = warden.authenticate!(auth_options)
 
@@ -20,19 +19,15 @@ class Users::SessionsController < Devise::SessionsController
       end
     end
   end
-
   private
-
   def continue_sign_in(resource, resource_name)
     set_flash_message!(:notice, :signed_in)
     sign_in(resource_name, resource)
     yield resource if block_given?
     respond_with resource, location: after_sign_in_path_for(resource)
   end
-
   def check_recaptcha
     return if verify_recaptcha # verify_recaptcha(action: 'login') for v3
-
     self.resource = resource_class.new sign_in_params
     respond_with_navigational(resource) do
       flash.discard(:recaptcha_error) # We need to discard flash to avoid showing it on the next page reload
