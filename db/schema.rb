5785d8498b81859536cad24e3f8484c26f8594db
Shamoonxpro <shamoon.khan@xprolabs.com>
2023-02-23 18:51:37 +0300

    Include Foreign key in resume and add fields in resume table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_23_075050) do
+ActiveRecord::Schema.define(version: 2023_02_23_133650) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2023_02_23_075050) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
+    t.text "description"
     t.index ["user_id"], name: "index_resumes_on_user_id"
   end
 
