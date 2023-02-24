35190342a5658f4a3daf3675bc5814bfed7f0078
Shamoonxpro <shamoon.khan@xprolabs.com>
2023-02-24 16:35:45 +0300

    Intendation of code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
-  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
  
   enum role: [:user,:admin]
-  validates :first_name, :last_name, :email, presence: true
+  validates :email, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :role, presence: true
   has_one_time_password
   enum otp_module: { disabled: 0, enabled: 1 }, _prefix: true
