a22459886c37ea8ecf2ed2cd8df7f7e23f535e3a
Shamoonxpro <shamoon.khan@xprolabs.com>
2023-02-27 21:06:58 +0300

    Add fuction that user update his detail without enter password

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   prepend_before_action :check_recaptcha, only: [:create] # Change this to be any actions you want to protect.
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
   private
   def check_recaptcha
     return if verify_recaptcha # verify_recaptcha(action: 'signup') for v3
