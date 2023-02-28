91e845ff566e74f542d9588efcc1c86569449365
Shamoonxpro <shamoon.khan@xprolabs.com>
2023-02-28 12:03:38 +0300

    Defien the routes for registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    registrations: "users/registrations" 
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
   get "users/show"
+  put '/users/update', to: 'registrations#update', as: :update_user
 
   resources :resumes
   resources :users do
